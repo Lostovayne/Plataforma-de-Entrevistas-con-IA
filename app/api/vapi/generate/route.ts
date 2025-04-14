ba5ff1c889b5b5e239708bdc998408ea4c842402
LostoVayne <moises.barrera.dev@gmail.com>
2025-04-13 20:23:53 -0400

    feat(generate): update interview generation to save to Firebase
    Added functionality to save generated interview questions to Firebase. Updated the AI model to `gemini-2.0-flash-001` and improved error handling. Also, configured Next.js to ignore ESLint and TypeScript errors during builds.

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,3 +1,5 @@
+import { db } from '@/firebase/admin';
+import { getRandomInterviewCover } from '@/lib/utils';
 import { google } from '@ai-sdk/google';
 import { generateText } from 'ai';
 
@@ -9,8 +11,8 @@ export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
 
   try {
-    const { text } = await generateText({
-      model: google('gemini-2.5-pro-exp-03-25'),
+    const { text: questions } = await generateText({
+      model: google('gemini-2.0-flash-001'),
       prompt: `El rol del trabajo es ${role}.
       El nivel de experiencia del trabajo es ${level}.
       La pila tecnológica usada en el trabajo es ${techstack}.
@@ -19,10 +21,26 @@ export async function POST(request: Request) {
       Por favor, devuelve únicamente las preguntas, sin ningún texto adicional.
       Las preguntas serán leídas por un asistente de voz, así que no uses barras ni asteriscos ni otros símbolos.
       Formatea las preguntas así:
-      [Pregunta 1, Pregunta 2, Pregunta 3]
+      ["Pregunta 1", "Pregunta 2" ,"Pregunta 3" ].
       ¡Gracias! <3`,
     });
+
+    const interview = {
+      role,
+      type,
+      level,
+      techstack: techstack.split(','),
+      questions: JSON.parse(questions),
+      userId: userid,
+      finalized: true,
+      coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
+
+    await db.collection('interviews').add(interview);
+    return Response.json({ success: true }, { status: 200 });
   } catch (error) {
+    console.log(error);
     return Response.json({ success: false, error }, { status: 500 });
   }
 }
